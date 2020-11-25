fff18d5c84bd84f835822a5514ae3e0a0410be9f
Michael L. Mehr <mike@azuresults.com>
2020-11-25 12:00:47 -0800

    Initial fixes to get to green (compile)
    Fixed compiler errors, including use of override.
    Added ISensorControl classes (interface and impl) to enable
    compilation.

--- Adjuster.cpp
+++ Adjuster.cpp
@@ -1,6 +1,9 @@
 // current to LED
 // sensor luminance (photo diode)
 
+#include <cmath>
+using std::abs;
+
 /*
 IAdjuster
     double SetLuminance(double value);
@@ -11,18 +14,36 @@ inject ISensorControl
     double GetLuminance();
     void SetCurrent(double value);
 */
+class ISensorControl {
+    public:
+    double GetMinCurrent() const;
+    double GetMaxCurrent() const;
+    double GetLuminance() const;
+    void SetCurrent(double value);
+};
+
+class ISensorControlMock : public ISensorControl {
+    //
+    double data;
+    public:
+    double GetMinCurrent() const { return 0.0; }
+    double GetMaxCurrent() const { return 255.0; }
+    double GetLuminance() const { return data; }
+    void SetCurrent(double value) { data = value; }
+};
 
 class IAdjuster {
+    public:
    IAdjuster() = default;
    // etc.
    virtual ~IAdjuster() = 0;
    virtual double SetLuminance(double value, double tolerance) = 0;
-}
+};
 
 class Adjuster : public IAdjuster {
    ISensorControl& sc;
-   void BSearchLum(double low, double hi, double target, double tolerance) {
-        avg = (low + hi)/2.; // need to use double divisor
+   double BSearchLum(double low, double hi, double target, double tolerance) {
+        double avg = (low + hi)/2.; // need to use double divisor
         sc.SetCurrent(avg);
         double lum = sc.GetLuminance();
         
@@ -35,10 +56,11 @@ class Adjuster : public IAdjuster {
           return BSearchLum(avg, hi, target, tolerance);
       }
 public:
-   Adjuster(const ISensorControl& sc_) : sc(sc_) {}
-   override double SetLuminance(double target, double tolerance) {
+   Adjuster(ISensorControl& sc_) : sc(sc_) {}
+   double SetLuminance(double target, double tolerance) override {
        // use sc to set current values and zero in on proper luminance
        
       return BSearchLum(sc.GetMinCurrent(), sc.GetMaxCurrent(), target, tolerance);
     }
-}
+};
+
