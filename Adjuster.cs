c625fc8fbea8d186821a52f3f03c8192e316fd38
Michael L. Mehr <mike@azuresults.com>
2020-11-28 11:27:48 -0800

    Initial conversion to C#
    Create new project, rename Adjuster.cpp to *.cs and add,
    basic syntax conversion for classes ISensorControl and
    IAdjuster and derivatives.
    Does not compile Adjuster.cs properly.

--- Adjuster.cs
+++ Adjuster.cs
@@ -1,9 +1,6 @@
 // current to LED
 // sensor luminance (photo diode)
 
-#include <cmath>
-using std::abs;
-
 /*
 IAdjuster
     double SetLuminance(double value);
@@ -14,34 +11,29 @@ inject ISensorControl
     double GetLuminance();
     void SetCurrent(double value);
 */
-class ISensorControl {
-    public:
-    double GetMinCurrent() const;
-    double GetMaxCurrent() const;
-    double GetLuminance() const;
-    void SetCurrent(double value);
+abstract class ISensorControl {
+    public abstract double GetMinCurrent();
+    public abstract double GetMaxCurrent();
+    public abstract double GetLuminance();
+    public abstract void SetCurrent(double value);
 };
 
-class ISensorControlMock : public ISensorControl {
+class ISensorControlMock : ISensorControl {
     //
     double data;
-    public:
-    double GetMinCurrent() const { return 0.0; }
-    double GetMaxCurrent() const { return 255.0; }
-    double GetLuminance() const { return data; }
-    void SetCurrent(double value) { data = value; }
+
+    public override double GetMinCurrent() { return 0.0; }
+    public override double GetMaxCurrent() { return 255.0; }
+    public override double GetLuminance() { return data; }
+    public override void SetCurrent(double value) { data = value; }
 };
 
-class IAdjuster {
-    public:
-   IAdjuster() = default;
-   // etc.
-   virtual ~IAdjuster() = 0;
-   virtual double SetLuminance(double value, double tolerance) = 0;
+abstract class IAdjuster {
+    public abstract double SetLuminance(double value, double tolerance);
 };
 
-class Adjuster : public IAdjuster {
-   ISensorControl& sc;
+class Adjuster : IAdjuster {
+   ISensorControl sc;
    double BSearchLum(double low, double hi, double target, double tolerance) {
         double avg = (low + hi)/2.; // need to use double divisor
         sc.SetCurrent(avg);
@@ -55,9 +47,11 @@ class Adjuster : public IAdjuster {
        else
           return BSearchLum(avg, hi, target, tolerance);
       }
-public:
-   Adjuster(ISensorControl& sc_) : sc(sc_) {}
-   double SetLuminance(double target, double tolerance) override {
+
+   public Adjuster(ISensorControl sc_) {
+        sc = sc_;
+    }
+    public override double SetLuminance(double target, double tolerance) {
        // use sc to set current values and zero in on proper luminance
        
       return BSearchLum(sc.GetMinCurrent(), sc.GetMaxCurrent(), target, tolerance);
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adjuster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
