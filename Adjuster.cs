90efe31ad9e35486f78b0559868ec27701b1df5e
Michael L. Mehr <mike@azuresults.com>
2020-11-28 11:38:21 -0800

    Fix compilation errors
    Needed to use System and rename abs() to Math.Abs()
    It also didn't like using "/2." as a double - thought was Int32

--- Adjuster.cs
+++ Adjuster.cs
@@ -11,50 +11,54 @@ inject ISensorControl
     double GetLuminance();
     void SetCurrent(double value);
 */
-abstract class ISensorControl {
-    public abstract double GetMinCurrent();
-    public abstract double GetMaxCurrent();
-    public abstract double GetLuminance();
-    public abstract void SetCurrent(double value);
-};
+using System;
 
-class ISensorControlMock : ISensorControl {
-    //
-    double data;
+namespace adjuster {
+    abstract class ISensorControl {
+        public abstract double GetMinCurrent();
+        public abstract double GetMaxCurrent();
+        public abstract double GetLuminance();
+        public abstract void SetCurrent(double value);
+    };
 
-    public override double GetMinCurrent() { return 0.0; }
-    public override double GetMaxCurrent() { return 255.0; }
-    public override double GetLuminance() { return data; }
-    public override void SetCurrent(double value) { data = value; }
-};
+    class ISensorControlMock : ISensorControl {
+        //
+        double data;
 
-abstract class IAdjuster {
-    public abstract double SetLuminance(double value, double tolerance);
-};
+        public override double GetMinCurrent() { return 0.0; }
+        public override double GetMaxCurrent() { return 255.0; }
+        public override double GetLuminance() { return data; }
+        public override void SetCurrent(double value) { data = value; }
+    };
 
-class Adjuster : IAdjuster {
-   ISensorControl sc;
-   double BSearchLum(double low, double hi, double target, double tolerance) {
-        double avg = (low + hi)/2.; // need to use double divisor
-        sc.SetCurrent(avg);
-        double lum = sc.GetLuminance();
-        
-       if (abs(lum - target) < tolerance) {
-         return  lum;
+    abstract class IAdjuster {
+        public abstract double SetLuminance(double value, double tolerance);
+    };
+
+    class Adjuster : IAdjuster {
+        ISensorControl sc;
+        double BSearchLum(double low, double hi, double target, double tolerance) {
+            double avg = (low + hi) / 2.0; // need to use double divisor
+            sc.SetCurrent(avg);
+            double lum = sc.GetLuminance();
+
+            if (Math.Abs(lum - target) < tolerance) {
+                return lum;
+            }
+            else if (lum > target)
+                return BSearchLum(low, avg, target, tolerance);
+            else
+                return BSearchLum(avg, hi, target, tolerance);
         }
-        else if (lum > target)
-          return BSearchLum(low, avg, target, tolerance);
-       else
-          return BSearchLum(avg, hi, target, tolerance);
-      }
 
-   public Adjuster(ISensorControl sc_) {
-        sc = sc_;
-    }
-    public override double SetLuminance(double target, double tolerance) {
-       // use sc to set current values and zero in on proper luminance
-       
-      return BSearchLum(sc.GetMinCurrent(), sc.GetMaxCurrent(), target, tolerance);
-    }
+        public Adjuster(ISensorControl sc_) {
+            sc = sc_;
+        }
+        public override double SetLuminance(double target, double tolerance) {
+            // use sc to set current values and zero in on proper luminance
+
+            return BSearchLum(sc.GetMinCurrent(), sc.GetMaxCurrent(), target, tolerance);
+        }
+    };
+
 };
-
